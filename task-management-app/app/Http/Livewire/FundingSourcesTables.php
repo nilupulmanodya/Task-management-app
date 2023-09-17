55aedddb3ff99b2477c1b70d9b85c52442ed07e4
Nadun Madhushanka <madushankaforgit@gmail.com>
2023-09-17 13:03:50 +0530

    started make tables

---
+++ task-management-app/app/Http/Livewire/FundingSourcesTables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class FundingSourcesTables extends Component
+{
+    public function render()
+    {
+        return view('livewire.funding-sources-tables');
+    }
+}
--- task-management-app/resources/views/funding_tables.blade.php
+++ task-management-app/resources/views/funding_tables.blade.php
@@ -8,10 +8,10 @@
     </x-slot>
 
     <div class="py-12">
+
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                {{-- <livewire:funding-sources /> --}}
-                <livewire:datatable model="App\Models\FundingSource" />
+                <livewire:funding-sources-tables/>
             </div>
         </div>
     </div>
